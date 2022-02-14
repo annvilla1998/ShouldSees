611f3548fcdcec5ff102e766c57bca9c6359736c
Sharon Fang <sharonfang8@gmail.com>
2022-02-14 15:53:22 -0800

    deleted old models and migrations and created new user model and seeder

--- express-project-starter/db/models/user.js
+++ express-project-starter/db/models/user.js
@@ -1,27 +1,23 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      User.hasMany(models.MyShowList, { foreignKey: 'userId' });
-      User.hasMany(models.Review, { foreignKey: 'userId' });
-    }
-  }
-  User.init({
-    username: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  const User = sequelize.define('User', {
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+      unique: true
+    },
+    hashedPassword: {
+      allowNull: false,
+      type: DataTypes.STRING.BINARY
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING(255),
+      unique: true
+    },
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
   return User;
 };
