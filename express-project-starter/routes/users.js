23f70c2cea9ffe714b2244be09437d90a7d53acd
shams <shamsjshaikh@gmail.com>
2022-02-14 18:58:11 -0600

    added router for users, not complete

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -11,8 +11,25 @@ const db = require('../db/models');
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
 router.get('/register', function (req, res, next) {
-  res.send('respond with a resource');
+  res.render('user-register')
 });
+
+router.post('/register', (req, res, next) => {
+  const {
+    username,
+    email,
+    password
+  } = req.body;
+
+  let user = db.User.build({
+    username,
+    email
+
+  })
+
+})
+
 
 module.exports = router;
