6c5a35d49154b720231f7b2829cd5f4f3cd02cf7
shams <shamsjshaikh@gmail.com>
2022-02-14 18:31:50 -0600

    created user and util pug first try

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../db/models');
+
+
+
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/register', function (req, res, next) {
   res.send('respond with a resource');
 });
 
