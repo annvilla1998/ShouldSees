49efcd112956fd1edbd473abc97a467f5bd3b8cf
Sharon Fang <sharonfang8@gmail.com>
2022-02-17 20:25:41 -0800

    still figuring out how to get into my shows

--- routes/my-show-list.js
+++ routes/my-show-list.js
@@ -5,42 +5,27 @@ const { csrfProtection, asyncHandler } = require('./utils');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const myshowlistshow = require('../db/models/myshowlistshow');
-const { User, MyShowList, Show } = db;
+const { requireAuth } = require('../auth.js')
+const { User, MyShowList, Show, MyShowListShow } = db;
 
-router.get('/', asyncHandler(async (req, res) => {
+router.get('/', requireAuth, asyncHandler(async (req, res) => {
+    const { userId } = req.session.auth;
 
-    // const showList = db.myShowList.build();
-    // const myShows = await MyShowList.findAll({
-    //     include: {
-    //         model: MyShowList,
-    //         include: Show
-    //     }
-    // });
+    const user = await User.findByPk(1, {
+        include: {
+            model: MyShowList,
+            include: Show
+        }
+    })
 
+    const showLists = user.MyShowLists;
+    const shows = showLists.Shows;
 
+    console.log("&&&&&&&&&&&&&&&&&&", showLists.MyShowList)
 
-    // console.log('&&&&&&&&&&&&&&&&', myShows.Show);
+    res.render('my-show-list.pug', {
 
-    res.render('my-show-list'
-    )
-
-
-
-    const { currWatch, watched, wantWatch, userId } = req.body;
-    // const { myShowListId } = req.body;
-
-    // console.log('req.body', req.body);
-
-    // const shows = await db.Show.findAll({
-    //     where: { myShowListId }
-    // })
-
-    // const myShowList = await db.MyShowList.findAll({
-    //     where: { userId }
-    // })
-    // console.log('am i in here')
-    // res.render('my-show-list.pug');
-
+    })
 
 }))
 
