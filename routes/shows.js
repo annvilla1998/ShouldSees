bbe6e6a8a78337b5becf983bec0409dba518bd7c
Anabel <villalobos1998@gmail.com>
2022-02-17 16:19:30 -0800

    added edit and delete review button

--- routes/shows.js
+++ routes/shows.js
@@ -21,66 +21,20 @@ router.get('/:id(\\d+)', asyncHandler(async(req,res) => {
     const show = await db.Show.findByPk(showId)
     const reviews = await Review.findAll({
         where: {
-            showsId: showId
+            showsId: showId,
+        },
+        include: {
+            model: User
         }
     })
-    console.log(reviews)
+    console.log(reviews.User.username)
+  
     res.render('show-details.pug', {
         show,
         showId,
-        reviews
+        reviews,
     })
 }))
-
-/*
-router.get('/:id(\\d+)/review', requireAuth, csrfProtection, asyncHandler(async(req,res)=> {
-
-    const showId = parseInt(req.params.id, 10);
-    const show = await db.Show.findByPk(showId)
-    const review = await Review.build();
-    res.render("review.pug", {
-        show,
-        showId,
-        review,
-        csrfToken: req.csrfToken()
-    })
-}))
-
-const reviewValidators = [
-    check('review')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for username')
-]
-
-router.post('/:id(\\d+)/review', reviewValidators, csrfProtection, asyncHandler(async(req,res)=> {
-    const showId = parseInt(req.params.id, 10);
-    const show = await db.Show.findByPk(showId)
-
-
-    const { content, rating } = req.body;
-
-    const review = await Review.create({ userId: res.locals.user.id, content, showId, rating });
-    console.log(req.body)
-    // const validatorErrors = validationResult(req);
-
-    // if (validatorErrors.isEmpty()) {
-    //   await review.save();
-      res.redirect(`/shows/${showId}`);
-    // } else {
-    //   const errors = validatorErrors.array().map((error) => error.msg);
-    //   res.render('review.pug', {
-    //     show,
-    //     review,
-    //     errors,
-    //     csrfToken: req.csrfToken()
-    //   });
-    // }
-}))
-
-router.patch('/:id(\\d+)/review/edit')
-
-// router.delete('/:id(\\d+)/review/:id')
-*/
 
 const reviewValidators = [
     check('review')
@@ -113,5 +67,10 @@ router.post('/:id(\\d+)/review', requireAuth, csrfProtection, asyncHandler(async
     await review.save();
     res.redirect(`/shows/${review.showsId}`);
 }))
+
+router.get('/review/:id(\\d+)/edit', csrfProtection)
+
+// router.delete('/:id(\\d+)/review/:id(\\d+)/delete')
+
 
 module.exports = router;
