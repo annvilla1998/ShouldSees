631f7b84cbe801117bf627a8d8f447a1e14f5d63
Sharon Fang <sharonfang8@gmail.com>
2022-02-17 16:27:33 -0800

    deleted commented out stuff in shows.js route

--- routes/shows.js
+++ routes/shows.js
@@ -40,61 +40,6 @@ router.get('/:id(\\d+)', asyncHandler(async(req,res) => {
     });
 }))
 
-/*
-router.get('/:id(\\d+)/review', requireAuth, csrfProtection, asyncHandler(async(req,res)=> {
-
-    const showId = parseInt(req.params.id, 10);
-    const show = await db.Show.findByPk(showId)
-    const review = await Review.build();
-    res.render("review.pug", {
-        show,
-        showId,
-        review,
-        csrfToken: req.csrfToken()
-    })
-}))
-
-const reviewValidators = [
-    check('review')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for username')
-]
-
-router.post('/:id(\\d+)/review', reviewValidators, csrfProtection, asyncHandler(async(req,res)=> {
-    const showId = parseInt(req.params.id, 10);
-    const show = await db.Show.findByPk(showId)
-
-
-    const { content, rating } = req.body;
-
-    const review = await Review.create({ userId: res.locals.user.id, content, showId, rating });
-    console.log(req.body)
-    // const validatorErrors = validationResult(req);
-
-    // if (validatorErrors.isEmpty()) {
-    //   await review.save();
-      res.redirect(`/shows/${showId}`);
-    // } else {
-    //   const errors = validatorErrors.array().map((error) => error.msg);
-    //   res.render('review.pug', {
-    //     show,
-    //     review,
-    //     errors,
-    //     csrfToken: req.csrfToken()
-    //   });
-    // }
-}))
-
-router.patch('/:id(\\d+)/review/edit')
-
-// router.delete('/:id(\\d+)/review/:id')
-*/
-
-const reviewValidators = [
-    check('review')
-    .exists({ checkFalsy: true })
-    .withMessage('Please provide a value for username')
-]
 
 router.get('/:id(\\d+)/review', requireAuth, csrfProtection, asyncHandler(async(req,res)=> {
 
