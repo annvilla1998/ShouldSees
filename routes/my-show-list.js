2e53025d00d1d1b5b0c755fdbd5725b52821254a
Ty <ty.rickers@gmail.com>
2022-02-18 05:15:13 -0700

    used sequelize to grab all of users shows, still need to divide it up by currWatch, Watched, and watching

--- routes/my-show-list.js
+++ routes/my-show-list.js
@@ -7,9 +7,11 @@ const bcrypt = require('bcryptjs');
 const myshowlistshow = require('../db/models/myshowlistshow');
 const { requireAuth } = require('../auth.js')
 const { User, MyShowList, Show, MyShowListShow } = db;
+const { Op } = require("sequelize");
 
 router.get('/', requireAuth, asyncHandler(async (req, res) => {
     const { userId } = req.session.auth;
+    //use UserId in final findByPk. Just using 1 for testing.
 
     const user = await User.findByPk(1, {
         include: {
@@ -18,14 +20,34 @@ router.get('/', requireAuth, asyncHandler(async (req, res) => {
         }
     })
 
-    const showLists = user.MyShowLists;
-    const shows = showLists.Shows;
+    const want = await User.findByPk(1, {
+        include: {
+            model: MyShowList,
+            include: Show,
+        },
+            where: {
+                listName: "Want to Watch"
+            },
+    })
 
-    // console.log("&&&&&&&&&&&&&&&&&&", showLists.MyShowList)
+    // console.log('showList', user.MyShowLists)
+    for (let showlist of user.MyShowLists) {
+        console.log('kdkdkd', showlist.Shows);
+    }
+    //^^above code grabs all of John Wicks shows but need to divide them up by watch,want,curr
 
-    // res.render('my-show-list.pug', {
 
-    // })
+    // for (let wantList of want.MyShowLists) {
+    //     console.log('two show', wantList.Shows)
+    // }
+    //^^^ this still grabs all the shows
+
+    // const showLists = user.MyShowLists;
+    // const shows = showLists.Shows;
+
+    res.render('my-show-list.pug', {
+
+    })
 
 }))
 
